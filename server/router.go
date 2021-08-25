e58a413b46195016069498016ce48fb76c141be4
JanCalebManzano <jcmanzano37@gmail.com>
2021-08-25 10:04:14 +0800

    [add-api-package][Summary]: Add user and todo routes in router

--- server/router.go
+++ server/router.go
@@ -1,6 +1,11 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"goland-demo/api/controllers"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 // NewRouter is the constructor of the router
 func NewRouter() (r *gin.Engine) {
@@ -10,6 +15,22 @@ func NewRouter() (r *gin.Engine) {
 	r.Use(logger())
 	r.Use(cors())
 	r.Use(jsonify())
+
+	userCtrl, err := controllers.NewUserController()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	userRtr := r.Group("/users")
+	userRtr.GET("/", userCtrl.GetUsers)
+	userRtr.GET("/0", userCtrl.GetUser)
+
+	todoCtrl, err := controllers.NewTodoController()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	todoRtr := r.Group("/todos")
+	todoRtr.GET("/", todoCtrl.GetTodos)
+	todoRtr.GET("/0", todoCtrl.GetTodo)
 
 	return r
 }
